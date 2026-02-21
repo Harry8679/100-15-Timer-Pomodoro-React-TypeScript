ffe297ce09578164bd0d289307696ae1582186d8
Harry7986 <emarh.harry.code@gmail.com>
2026-02-21 13:33:34 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { PomodoroTimer } from './components/PomodoroTimer';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <PomodoroTimer />;
 }
 
-export default App
+export default App;
